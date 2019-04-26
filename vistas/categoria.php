382c701ba27197282e3bad743722dbd0d3b2a471
Scriptnet <alex147f1@gmail.com>
2019-04-26 14:11:03 -0500

    listar,editar, guardar condición

---
+++ vistas/categoria.php
@@ -0,0 +1,66 @@
+<?php
+require 'header.php';
+?>
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">        
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Categoría <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
+                        <div class="box-tools pull-right">
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <div class="panel-body table-responsive" id="listadoregistros">
+                        <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
+                          <thead>
+                            <th>Opciones</th>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Estado</th>
+                          </thead>
+                          <tbody>                            
+                          </tbody>
+                          <tfoot>
+                            <th>Opciones</th>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Estado</th>
+                          </tfoot>
+                        </table>
+                    </div>
+                    <div class="panel-body" style="height: 400px;" id="formularioregistros">
+                        <form name="formulario" id="formulario" method="POST">
+                          <div class="form-grup col-lg-6 col-md-6 col-xs-12" >
+                            <label>Nombre:</label>
+                            <input type="hidden" name="idcategoria" id="idcategoria" >
+                            <input type="text" class="form-control" name="nombre" id="nombre" maxlength="50" placeholder="Nombre" required>
+                          </div>
+                          <div class="form-grup col-lg-6 col-md-6 col-xs-12" >
+                            <label>Descripción:</label>
+                            <input type="text " class="form-control" name="descripcion" id="descripcion" maxlength="256" placeholder="Descripción">
+                          </div>
+                         
+                            <button class="btn btn-primary" type="submit" name="btnGuardar"> Guardar <i class="fa fa-save"></i></button>
+                             <button class="btn btn-danger" onclick="cancelarform()" type="button">Cancelar <i class="fa fa-close"></i></button>
+                          
+
+                        </form>
+                    </div>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+      </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
+<?php
+require 'footer.php';
+?>
+<script type="text/javascript" src="categoria.js"></script>
